f4e89404f464306636099a421eeb8f2e9963d9a0
Admin <Admin@TEB123001>
2022-10-19 11:47:25 +0200

    Add project files.

---
+++ Uppgift 4_22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_4_22
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            for(int i = 10; i <= 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int j = 200; j >= 180; j--)
+            {
+                Console.WriteLine(j);
+
+            }
+
+            for (int m = 1000; m <= 1400; m = m + 50)
+            {
+                Console.WriteLine(m);
+            }
+
+        }
+    }
+}
+
